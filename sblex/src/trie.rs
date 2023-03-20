7234e24b4ccd16424e1c8dba48ecb6e059959ccc
Kristoffer Andersson <kod.kristoff@gmail.com>
2023-03-20 09:20:08 +0100

    fix: update

--- sblex/src/trie.rs
+++ sblex/src/trie.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use unicode_segmentation::UnicodeSegmentation;
+use unicode_segmentation::{Graphemes, UnicodeSegmentation};
 
 pub struct Trie {
     trie: HashMap<usize, (HashMap<String, usize>, String)>,
@@ -28,6 +28,7 @@ impl Trie {
 
 pub struct TrieBuilder {
     count: usize,
+    state: usize,
     trie: HashMap<usize, (HashMap<String, usize>, Vec<String>)>,
 }
 
@@ -35,6 +36,7 @@ impl Default for TrieBuilder {
     fn default() -> Self {
         TrieBuilder {
             count: 0,
+            state: 0,
             trie: HashMap::new(),
         }
     }
@@ -48,12 +50,31 @@ impl TrieBuilder {
     pub fn insert(&mut self, word: &str, decoration: String) {
         self.count += 1;
         let mut st = 0;
-        for c in word.graphemes(true) {
-            st = match self.trie.get(st) {
+        let mut iter = word.graphemes(true);
+        loop {
+            // for c in word.graphemes(true) {
+            let c = match iter.next() {
+                Some(c) => c,
+                None => break,
+            };
+            st = match self.trie.get(&st) {
                 Some(tuple) => match tuple.0.get(c) {
                     Some(state) => *state,
+                    None => todo!(),
                 },
+                None => {
+                    return self.complete(st, iter, decoration);
+                }
             };
+        }
+        self.trie.get_mut(&st).unwrap().1.push(decoration);
+    }
+
+    // create a new branch
+    fn complete(&mut self, st: usize, word: Graphemes, decoration: String) {
+        for c in word {
+            self.state += 1;
+            self.trie.get_mut(&st).expect("st exists").0.get_mut(c)
         }
     }
 }
