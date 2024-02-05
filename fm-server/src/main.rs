f4784dc37bd089aa680bf71ba0010ca43bd84435
Kristoffer Andersson <kristoffer.andersson@gu.se>
2024-02-05 14:34:43 +0100

    feat: add cmd-line args

--- fm-server/src/main.rs
+++ fm-server/src/main.rs
@@ -1,12 +1,37 @@
+use std::path::PathBuf;
+
+use clap::Parser;
 use fm_server::{server, startup, state::AppState};
 
 #[tokio::main]
 async fn main() {
-    let state = AppState::from_path("assets/saldo.lex").unwrap();
+    let args = Options::parse();
+
+    let state = AppState::from_path(&args.saldo_morph_path).unwrap();
 
     let app = server::create_app(state);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let address = format!("{}:{}", args.host, args.port);
+    eprintln!("Starting server at 'http://{}'", address);
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
 
     startup::run(listener, app).await.unwrap();
+}
+
+#[derive(Debug, Clone, clap::Parser)]
+#[command(author, version, about, long_about=None)]
+struct Options {
+    /// The host to bind the server to.
+    #[arg(long, env = "FM_SERVER_APP_HOST", default_value = "127.0.0.1")]
+    host: String,
+    /// The port to bind the server to.
+    #[arg(long, env = "FM_SERVER_APP_PORT", default_value = "3000")]
+    port: u16,
+    /// The path to the morphology data for saldo
+    #[arg(
+        long,
+        env = "FM_SERVER_MORPH_SALDO_PATH",
+        default_value = "assets/testing/saldo.lex"
+    )]
+    saldo_morph_path: PathBuf,
 }
